e8c9674500a153171d6ca9d044aab93bf7d3b36a
Alexandre Sebrao <alexandre.sebrao@gmail.com>
2017-01-11 16:40:43 -0200

    Inicio

--- inicio/models.py
+++ inicio/models.py
@@ -1,5 +1,65 @@
 from __future__ import unicode_literals
-
+from pagseguro.api import PagSeguroApiTransparent, PagSeguroItem
 from django.db import models
 
-# Create your models here.
+
+class PaymentPagSeguro(models.Model):
+    TYPE_PAYMENT = (
+        ('1', 'boleto'),
+        ('2', 'cartão'),
+    )
+    sender_name = models.TextField()
+    sender_area_code = models.IntegerField()
+    sender_phone = models.CharField(max_length=15)
+    sender_email = models.TextField()
+    sender_cpf = models.CharField(max_length=11)
+    shipping_street = models.TextField()
+    shipping_number = models.IntegerField(blank=True, null=True)
+    shipping_complement = models.TextField(blank=True, null=True)
+    shipping_district = models.TextField()
+    shipping_postalcode = models.CharField(max_length=8)
+    shipping_city = models.TextField()
+    shipping_state = models.CharField(max_length=2)
+    shipping_country = models.CharField(max_length=3)
+    payment_type = models.CharField(choices=TYPE_PAYMENT, max_length=1)
+    pagseguro_code = models.TextField()
+
+    def sender_dictionary(self):
+        return {'name': self.sender_name, 'area_code': self.area_code,
+                'phone': self.sender_phone, 'email': self.email,
+                'cpf': self.cpf}
+
+    def shipping_dictionary(self):
+        return {'street': self.shipping_street, 'number': self.shipping_number,
+                'complement': self.shipping_complement,
+                'district': self.shipping_district,
+                'postal_code': self.shipping_postalcode,
+                'city': self.shipping_city,
+                'state': self.shipping_state, 'country': self.shipping_country}
+
+    def checkout(self, sender_hash):
+        # Instancia a parte da api do Pagseguro
+        api = PagSeguroApiTransparent()
+
+        # Adicionamos os Items comprados
+        for item in self.itempayment_set.all():
+            api.add_item(PagSeguroItem(item.dictionary()))
+
+        api.set_sender(**self.sender_dictionary())
+        api.set_shipping(**self.shipping_dictionary())
+        if (self.payment_type == 1):
+            api.set_payment_method('boleto')
+        data = api.checkout()
+        data['']
+
+
+class ItemPayment(models.Model):
+    item_id = models.IntegerField()
+    item_description = models.TextField()
+    item_quantity = models.IntegerField()
+    item_amount = models.FloatField()
+    payment_pagseguro = models.ForeignKey(PaymentPagSeguro)
+
+    def dictionary(self):
+        return {'id': self.item_id, 'item_description': self.item_description,
+                'amount': self.item_amout, 'quantity': self.quantity}
--- inicio/views.py
+++ inicio/views.py
@@ -15,25 +15,19 @@ def index(request):
 
 
 def checkout(request):
-    # api = PagSeguroApiTransparent()
-    # sender = {'name': 'Jose Comprador', 'area_code': 11, 'phone': 56273440,
-    #           'email': 'comprador@uol.com.br', 'cpf': '22111944785'}
-    # api.set_sender(**sender)
-    # shipping = {'street': "Av. Brigadeiro Faria Lima", 'number': 1384,
-    #             'complement': '5o andar', 'district': 'Jardim Paulistano',
-    #             'postal_code': '01452002', 'city': 'Sao Paulo', 'state': 'SP',
-    #             'country': 'BRA'}
-    # api.set_shipping(**shipping)
-    # item1 = PagSeguroItem(id='0001', description='Notebook Prata',
-    #                       amount='24300.00', quantity=1)
-    # api.set_payment_method('boleto')
-    # api.set_sender_hash(request.POST['sender_hash'])
-    # api.checkout()
-    # context = {'user': request.user}
-    # data = PagSeguroApiTransparent().get_session_id()
-    print request.POST['sender_hash']
+    api = PagSeguroApiTransparent()
+    if request.POST['payment'] == "boleto":
+        api.set_payment_method('boleto')
+        api.set_sender_hash(request.POST['sender_hash'])
+        # Item
+        item1 = PagSeguroItem(id='0001', description='Anucio', amount='39.00',
+                              quantity=1)
+        # Dados do Comprador
+        sender = {'name': 'Jose Comprador', 'area_code': 11, 'phone': 56273440,
+                  'email': 'comprador@sandbox.pagseguro.com.br', 'cpf': '22111944785'}
+        api.set_sender(**sender)
 
-    context = {'sender_hash': request.POST['sender_hash']}
-    template = 'index.html'
-
-    return render(request, template, context)
+        api.add_item(item1)
+        shipping = {'street': "Av. Brigadeiro Faria Lima", 'number': 1384, 'complement': '5o andar', 'district': 'Jardim Paulistano', 'postal_code': '01452002', 'city': 'Sao Paulo', 'state': 'SP', 'country': 'BRA',}
+        api.set_shipping(**shipping)
+        data = api.checkout()
