5f1e8c989d95625140525da686edfb8c22874b9f
Alexandre Sebrao <alexandre.sebrao@gmail.com>
2017-01-12 19:02:24 -0200

    Controle de errors

--- inicio/views.py
+++ inicio/views.py
@@ -12,9 +12,12 @@ def index(request):
     if not data['success']:
         return HttpResponse('<h1>Error :()</h1>')
     context['session_id'] = data['session_id']
-    context['anos'] = range(datetime.datetime.now().year,
+    context['anos'] = range(datetime.datetime.now().year-1,
                             int(datetime.datetime.now().year)+20)
-
+    months = []
+    for i in range(1, 12):
+        months.append("%02d" % i)
+    context['months'] = months
     template = 'index.html'
     return render(request, template, context)
 
